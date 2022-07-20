f05d896387a3094b5980b0414872343ac28b42f5
Chris Conley <conley.christopher@hotmail.com>
2022-07-19 19:28:29 -0500

    commiting update to image

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
+import bgImg from '../images/netflix.jpg';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const {user, logIn} = UserAuth();
+  const {logIn} = UserAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -24,7 +25,7 @@ const Login = () => {
   return (
     <>
     <div className='w-full h-screen'>
-      <img className='hidden sm:block absolute w-full h-full object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/1ef84595-1fdb-4404-adac-15215ceeb3ae/9b7e4892-200e-4740-909b-cdd33763fe9f/US-en-20220711-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="/" />
+      <img className='hidden sm:block absolute w-full h-full object-cover' src={bgImg} alt="/" />
       <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
       <div className='fixed w-full px-4 py-24 z-50'>
         <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
